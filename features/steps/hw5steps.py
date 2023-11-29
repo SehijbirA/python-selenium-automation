1f9e1c4c193909f0e05664612a0b0ad15e974ce5
Sehij Aulakh <sehij@Sehijs-MacBook-Pro.local>
2023-11-28 18:25:36 -0800

    Homework 5

---
+++ features/steps/hw5steps.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+
+COLOR_OPTIONS = (By.CSS_SELECTOR, "[class*='ButtonWrapper'] img")
+SELECTED_COLOR = (By.CSS_SELECTOR, "[class*='StyledVariationSelectorImage'] [class*='CellVariationHeaderWrapper']")
+@given ('Open target product page A-88345426')
+def open_product_page_88345426 (context):
+    context.driver.get("https://www.target.com/p/A-88345426")
+
+@then ('Verify each color can be selected')
+def verify_each_color_can_be_selected (context):
+    expected_colors = ['Brown', 'Oatmeal', 'Gray', 'Black']
+    actual_colors = []
+
+    colors = context.driver.find_elements(*COLOR_OPTIONS)
+
+    for color in colors:
+        color.click()
+        selected_color = context.driver.find_element(*SELECTED_COLOR).text.split('\n')[1]
+        actual_colors.append(selected_color)
+
+    print(expected_colors)
+    print(actual_colors)
+    assert expected_colors == actual_colors
---
+++ features/tests/HW5.feature
@@ -0,0 +1,5 @@
+Feature: Target Loop test cases
+
+    Scenario: Verify there are 5 benefit boxes on the Target Circle page
+        Given Open target product page A-88345426
+        Then Verify each color can be selected
